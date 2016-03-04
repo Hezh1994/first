c96e0e979f7eb80a8be0e97bcb6de8433759ec41
Hezh1994 <hezh@succez.com>
2016-03-04 18:23:46 +0800

    修改了intToHex方法判断文件大小导致数据溢出；在关闭流时，对异常的处理

--- src/com/succez/first/FileToByte.java
+++ src/com/succez/first/FileToByte.java
@@ -3,8 +3,11 @@ package com.succez.first;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.logging.Logger;
 
 public class FileToByte {
+	private static Logger log = Logger.getLogger(FileToByte.class.getName());
+
 	/**
 	 * 将指定文件转换为一个字节数组并返回,当文件过大将无法进行转换
 	 * 
@@ -17,14 +20,14 @@ public class FileToByte {
 		ByteArrayOutputStream bos = null;
 		try {
 			if (file == null) {
-				return null;
+				throw new IllegalArgumentException("文件不存在");
 			}
-			if ((int) file.length() > Integer.MAX_VALUE) {
+			if (file.length() > Integer.MAX_VALUE) {
 				throw new Exception("文件太大，无法进行转换");
 			}
 			fis = new FileInputStream(file);
 			bos = new ByteArrayOutputStream((int) file.length());// 这里用1000会浪费很多临时内存空间
-			byte[] b = new byte[4096];// 读缓存最好一次读4096或者8192
+			byte[] b = new byte[406];// 读缓存最好一次读4096或者8192
 			int i;
 			while ((i = fis.read(b)) != -1) {
 				bos.write(b, 0, i);
@@ -32,11 +35,19 @@ public class FileToByte {
 			}
 			return bos.toByteArray();
 		} finally {
-			if (fis != null) {
-				fis.close();
+			try {
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (Exception e) {
+				log.severe("输入流关闭失败");
 			}
-			if (bos != null)
-				bos.close();
+			try {
+				if (bos != null)
+					bos.close();
+			} catch (Exception e) {
+				log.severe("输出流关闭失败");
+			}
 		}
 	}
 }
