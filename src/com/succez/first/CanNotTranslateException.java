dd5a0672be98758ec7095957504cb33f74e1fb14
Hezh1994 <hezh@succez.com>
2016-03-08 11:26:00 +0800

    自定义异常

---
+++ src/com/succez/first/CanNotTranslateException.java
@@ -0,0 +1,16 @@
+package com.succez.first;
+
+public class CanNotTranslateException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1218548432118578327L;
+
+	public CanNotTranslateException() {
+		super();
+	}
+
+	public CanNotTranslateException(String s) {
+		super(s);
+	}
+}
--- src/com/succez/first/WrongLevelException.java
+++ src/com/succez/first/WrongLevelException.java
@@ -1,7 +1,11 @@
 package com.succez.first;
 
 public class WrongLevelException extends Exception {
-	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -671054348042095498L;
 
 	public WrongLevelException() {
 		super();
